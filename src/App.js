099eb2274776e053f0fe768c94efd911eff1ab45
Dominika Moriak <dominika.moriak@gmail.com>
2023-04-19 22:04:46 +0200

    Create a new component - List, add code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Hero from './components/Hero/Hero.js';
 import SearchForm from './components/SearchForm/SearchForm.js';
+import List from './components/List/List.js';
 
 const App = () => {
   return (
     <div>
       <Hero />
       <SearchForm />
+      <List />
     </div>
   );
 };
---
+++ src/components/List/List.js
@@ -0,0 +1,23 @@
+const List = () => {
+    return (
+      <div>
+          <header>
+              <h2>Things to do<span>soon!</span></h2>
+          </header>
+          <p>Interesting things I want to check out</p>
+          <section>
+              <article>
+                  <h2>Books</h2>
+              </article>
+              <article>
+                  <h2>Movies</h2>
+              </article>
+              <article>
+                  <h2>Games</h2>
+              </article>
+          </section>
+      </div>
+    );
+  };
+  
+  export default List;
