a01a6715a797a9ada31ea39ad674f3c3f243d9ee
Dominika Moriak <dominika.moriak@gmail.com>
2023-04-20 14:10:49 +0200

    Create Hero.module.scss and import styles for this

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,8 +1,10 @@
+import styles from './Hero.module.scss';
+
 const Hero = () => {
     return (
-      <div>
-        <h1>My first React App</h1>
-        <p>A simple to-do app, with lists, columns and card</p>
+      <div className={styles.hero}>
+        <h1 className={styles.title}>My first React App</h1>
+        <p className={styles.subtitle}>A simple to-do app, with lists, columns and card</p>
       </div>
     );
   };
