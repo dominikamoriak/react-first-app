3d733f0281e7256af6cbcc69a812b8adae2aea93
Dominika Moriak <dominika.moriak@gmail.com>
2023-04-21 13:29:25 +0200

    Create ne wcomponent Container and add this to the App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import Hero from './components/Hero/Hero.js';
 import SearchForm from './components/SearchForm/SearchForm.js';
 import List from './components/List/List.js';
+import Container from './components/Container/Container.js';
 
 const App = () => {
   return (
-    <div>
+    <Container>
       <Hero />
       <SearchForm />
       <List />
-    </div>
+    </Container>
   );
 };
 
