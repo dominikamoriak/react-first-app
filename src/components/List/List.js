044d10dde61f0d54ec68ba3375a9901d4dae477d
Dominika Moriak <dominika.moriak@gmail.com>
2023-04-22 16:03:33 +0200

    Edit component List, add const value, setValue

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,8 +1,22 @@
 import styles from './List.module.scss';
 import Column from '../Column/Column';
-
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 const List = () => {
+    const [columns, setColumns] = useState([
+      { id: 1, title: 'Books', icon: 'book' },
+      { id: 2, title: 'Movies', icon: 'film' },
+      { id: 3, title: 'Games', icon: 'gamepad' }
+    ]);
+
+    const handleSubmit = e => {
+      e.preventDefault();
+      setColumns([...columns, { id: 4, title: 'Test' }]);
+    };
+
+    const [value, setValue] = useState('');
+
     return (
       <div className={styles.list}>
           <header className={styles.header}>
@@ -10,12 +24,17 @@ const List = () => {
           </header>
           <p className={styles.description}>Interesting things I want to check out</p>
           <section className={styles.columns}>
-            <Column title="Books" icon="book" />
-            <Column title="Movies" icon="film" />
-            <Column title="Games" icon="gamepad" />
+               {columns.map(column => <Column key={column.id} title={column.title} icon={column.icon} />)}
           </section>
+
+          <form onSubmit={handleSubmit}>
+	          <input type="text" value={value}
+            onChange={e => setValue(e.target.value)}/>
+		          <button>Add column</button>
+           </form>
       </div>
     );
   };
+  
   
   export default List;
