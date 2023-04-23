dc9f219127c5a7a7c9f895f3a2c5d2be36a9717c
Dominika Moriak <dominika.moriak@gmail.com>
2023-04-24 00:55:40 +0200

    Edit List

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,43 +1,10 @@
 import styles from './List.module.scss';
 import Column from '../Column/Column';
-import { useState } from 'react';
-import shortid from 'shortid';
 import ColumnForm from '../ColumnForm/ColumnForm';
+import { useSelector } from 'react-redux';
 
 const List = () => {
-    const [columns, setColumns] = useState([
-      { id: 1, title: 'Books', icon: 'book',
-      cards: [
-        { id: 1, title: 'This is Going to Hurt' },
-        { id: 2, title: 'Interpreter of Maladies' }
-      ]},
-      { id: 2, title: 'Movies', icon: 'film',
-      cards: [
-        { id: 1, title: 'Harry Potter' },
-        { id: 2, title: 'Star Wars' }
-      ]},
-      { id: 3, title: 'Games', icon: 'gamepad',
-      cards: [
-        { id: 1, title: 'The Witcher' },
-        { id: 2, title: 'Skyrim' }
-      ]},
-    ]);
-
-    const addColumn = newColumn => {
-      setColumns([...columns, { id: shortid(), title: newColumn.title, icon: newColumn.icon, cards: [] }]);
-    };
-
-    const addCard = (newCard, columnId) => {
-      const columnsUpdated = columns.map(column => {
-        if(column.id === columnId)
-          return { ...column, cards: [...column.cards, { id: shortid(), title: newCard.title }]}
-        else
-          return column
-      })
-    
-      setColumns(columnsUpdated);
-    
-    };
+  const columns = useSelector(state => state.columns);
 
     return (
       <div className={styles.list}>
