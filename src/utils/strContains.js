fe3dec86e59fc5f796cf7cdeb614b01af6eed2d7
Dominika Moriak <dominika.moriak@gmail.com>
2023-04-26 20:02:49 +0200

    Edit

--- src/utils/strContains.js
+++ src/utils/strContains.js
@@ -1,5 +1,6 @@
 const strContains = (str1, str2) => {
-    return str1.toLowerCase().includes(str2.toLowerCase());
+    console.log("str1: ", str1);
+    return str1.toLowerCase().includes(str2.toLowerCase()); 
   }
   
   export default strContains;
