01db1ac327f6c2761f69245ea327130bb3ba2231
Dominika Moriak <dominika.moriak@gmail.com>
2024-02-15 19:30:15 +0100

    Import Aboutt and Favorite to the App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Container from './components/Container/Container.js';
 import NavBar from './components/NavBar/NavBar.js';
 import Home from './components/Home/Home.js';
+import Favorite from './components/Favorite/Favorite.js';
+import About from './components/About/About.js';
 
 const App = () => {
   return (
@@ -8,6 +10,8 @@ const App = () => {
       <NavBar />
       <Container>
         <Home />
+        <About />
+        <Favorite />
       </Container>
     </main>
   );
--- src/components/Favorite/Favorite.js
+++ src/components/Favorite/Favorite.js
@@ -1,4 +1,4 @@
-import styles from './Favorite/module.scss';
+import styles from './Favorite.module.scss';
 import PageTitle from '../PageTitle/PageTitle';
 
 const Favorite = () => {
