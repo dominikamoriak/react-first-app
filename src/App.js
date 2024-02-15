6687f67917a0e5a79af5819a951aa1acdb66d24e
Dominika Moriak <dominika.moriak@gmail.com>
2024-02-15 19:17:41 +0100

    Create new component Home, take components Hero,SearchForm,List from App and get to the Home

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
-import Hero from './components/Hero/Hero.js';
-import SearchForm from './components/SearchForm/SearchForm.js';
-import List from './components/List/List.js';
 import Container from './components/Container/Container.js';
 import NavBar from './components/NavBar/NavBar.js';
+import Home from './components/Home/Home.js';
 
 const App = () => {
   return (
     <main>
       <NavBar />
       <Container>
-        <Hero />
-        <SearchForm />
-        <List />
+        <Home />
       </Container>
     </main>
   );
---
+++ src/components/Home/Home.js
@@ -0,0 +1,16 @@
+//import styles from './Home/Home';
+import Hero from '../Hero/Hero';
+import SearchForm from '../SearchForm/SearchForm';
+import List from '../List/List';
+
+const Home = () => {
+    return (
+        <>
+            <Hero />
+            <SearchForm />
+            <List />
+        </>
+    );
+};
+
+export default Home;
