2126a2c8ed9b9519fec177408811f8803528fc80
Dominika Moriak <dominika.moriak@gmail.com>
2024-02-16 15:43:28 +0100

    Create new component NoMatch for 404 not found and use it in Route in App

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './components/Home/Home.js';
 import Favorite from './components/Favorite/Favorite.js';
 import About from './components/About/About.js';
 import { Routes, Route } from 'react-router-dom';
+import NoMatch from './components/NoMatch/NoMatch.js';
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/favorite" element={<Favorite />} />
+          <Route path="*" element={<NoMatch />} />
         </Routes>
       </Container>
     </main>
---
+++ src/components/NoMatch/NoMatch.js
@@ -0,0 +1,9 @@
+import PageTitle from '../PageTitle/PageTitle';
+
+const NoMatch = () => (
+    <div>
+        <PageTitle>404 not found</PageTitle>
+    </div>
+);
+
+export default NoMatch;
