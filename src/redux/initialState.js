1dd04e271bfe24fe674d74cc844a5dfc9420afd0
Dominika Moriak <dominika.moriak@gmail.com>
2024-02-17 13:13:22 +0100

    Update initialState.js and add []lists

--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -1,20 +1,9 @@
 const initialState = {
     columns: [
-      {
-        id: 1,
-        title: 'Books',
-        icon: 'book',
-      },
-      {
-        id: 2,
-        title: 'Movies',
-        icon: 'film',
-      },
-      {
-        id: 3,
-        title: 'Games',
-        icon: 'gamepad',
-      }
+      { id: 1, listId: 1, title: 'Books', icon: 'book' },
+      { id: 2, listId: 1, title: 'Movies', icon: 'film' },
+      { id: 3, listId: 1, title: 'Games', icon: 'gamepad' },
+      { id: 4, listId: 2, title: 'Test list', icon: 'gamepad'}
     ],
   
     cards: [
@@ -24,6 +13,19 @@ const initialState = {
       { id: 4, columnId: 2, title: 'Star Wars' },
       { id: 5, columnId: 3, title: 'The Witcher' },
       { id: 6, columnId: 3, title: 'Skyrim' }
+    ],
+
+    lists: [
+      {
+        id: 1,
+        title: 'Things to do...',
+        description: 'Interesting things I want to check out'
+      },
+      {
+        id: 2,
+        title: 'Test list',
+        description: 'Lorem Ipsum'
+      }
     ],
 
     searchString: ''
