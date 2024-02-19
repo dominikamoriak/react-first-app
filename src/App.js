46de1d4b72506741c965a0a80f9bf31fcb36e5f0
Dominika Moriak <dominika.moriak@gmail.com>
2024-02-19 12:01:30 +0100

    Add new route path for /list

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Favorite from './components/Favorite/Favorite.js';
 import About from './components/About/About.js';
 import { Routes, Route } from 'react-router-dom';
 import NoMatch from './components/NoMatch/NoMatch.js';
+import List from './components/List/List.js';
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
           <Route path="/about" element={<About />} />
           <Route path="/favorite" element={<Favorite />} />
           <Route path="*" element={<NoMatch />} />
+          <Route path="/list/:listId" element={<List />} />
         </Routes>
       </Container>
     </main>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,6 @@
 //import styles from './Home/Home';
 import Hero from '../Hero/Hero';
 import SearchForm from '../SearchForm/SearchForm';
-import List from '../List/List';
 import Lists from '../Lists/Lists';
 
 const Home = () => {
