8baec0952cb5716689f0133df0cc31d45dd611aa
Dominika Moriak <dominika.moriak@gmail.com>
2024-02-19 14:02:25 +0100

    Edit Link in the comp.Lists-use list.id

--- src/components/Lists/Lists.js
+++ src/components/Lists/Lists.js
@@ -10,7 +10,7 @@ const Lists = () => {
         <section className={styles.lists}>
             <h2 className={styles.heading}>Browse lists</h2>
                 {lists.map(list => (
-                    <Link key={list.id} to="" className={styles.listLink}>
+                    <Link key={list.id} to={`/list/${list.id}`} className={styles.listLink}>
                         <h3>{list.title}</h3>
                         <p>{list.description}</p>
                     </Link>
