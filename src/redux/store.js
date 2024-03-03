471b72fe67ed006914c50561ab0f8adbcdf4ab66
Dominika Moriak <dominika.moriak@gmail.com>
2024-03-03 20:57:57 +0100

    Create subReducers in the store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -44,32 +44,54 @@ export const toggleCardFavorite = cardId => ({ type: 'TOGGLE_CARD_FAVORITE', pay
 
 
 const reducer = (state = initialState, action) => {
-    switch(action.type) {
+    const newState = {
+      lists: listsReducer(state.lists, action),
+      columns: columnsReducer(state.columns, action),
+      cards: cardsReducer(state.cards, action),
+      searchString: searchStringReducer(state.searchString, action)
+    };
 
-      case 'ADD_COLUMN':
-        return { ...state, columns: [...state.columns, 
-        { ...action.payload, id: shortid() }]};
-
-      case 'ADD_CARD':
-        return {...state, cards: [...state.cards, 
-        {...action.payload.card, id: shortid() }]};
-
-      case 'UPDATE_SEARCHSTRING':
-        return { ...state, searchString: action.payload };
-
-      case 'ADD_LIST':
-        return { ...state, lists: [...state.lists,
-        {...action.payload, id: shortid() }]};
-
-        case 'TOGGLE_CARD_FAVORITE':
-          return { ...state, cards: state.cards.map
-            (card => (card.id === action.payload) ? 
-            { ...card, isFavorite: !card.isFavorite } : card) };
-
-      default:
-        return state;
-    }
+    return newState;
   };
+
+  const listsReducer = (statePart = [], action) => {
+    switch(action.type) {
+      case 'ADD_LIST':
+        return [...statePart, {...action.payload, id: shortid() }];
+      default:
+        return statePart;
+    }
+  }
+
+  const columnsReducer = (statePart = [], action) => {
+    switch(action.type) {
+      case 'ADD_COLUMN':
+        return [...statePart, {...action.payload, id: shortid() }];
+      default:
+        return statePart;
+    }
+  }
+
+  const cardsReducer = (statePart = [], action) => {
+    switch(action.type) {
+      case 'ADD_CARD':
+        return [...statePart, {...action.payload.card, id: shortid() }];
+      case 'TOGGLE_CARD_FAVORITE':
+        return statePart.map(card => (card.id === action.payload) ?
+          {...card, isFavorite: !card.isFavorite} : card);
+      default:
+        return statePart;
+    }
+  }
+
+  const searchStringReducer = (statePart = '', action) => {
+    switch(action.type) {
+      case 'UPDATE_SEARCHSTRING':
+        return action.payload;
+      default:
+        return statePart;
+    }
+  }
 
 const store = createStore(
   reducer,
